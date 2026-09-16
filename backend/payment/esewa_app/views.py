00db58e49717aaa0d52d316e5f602d3db6750648
suyog khadka <ksuyog697@gmail.com>
2026-09-16 22:30:28 +0545

    buidsiness llogic cvhange

--- backend/payment/esewa_app/views.py
+++ backend/payment/esewa_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Cart, CartItem, Product
+from .models import *
 from .serializers import CartSerializer, ProductSerializer
 from django.conf import settings
 import uuid
@@ -75,7 +75,6 @@ class CartDetailAPIView(APIView):
                 hashlib.sha256,
             ).digest()
         ).decode()
-
         payment = {
             "amount": amount,
             "tax_amount": vat,
